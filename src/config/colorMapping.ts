8803550a15193618d044ddb1dcb43d2843a3ffce
tonyo <arabit.tr@gmail.com>
2026-08-19 08:43:51 +0800

    UPL: order of the group colors

--- src/config/colorMapping.ts
+++ src/config/colorMapping.ts
@@ -1,4 +1,12 @@
 export const colorGroups: Record<string, Record<string, string>> = {
+  "Admin": {
+    "Base Color": "jjjei_admin:0.css",
+    "Building and Land Administration (BLA)": "jjjei_admin:1.css",
+    "Compliance": "jjjei_admin:2.css",
+    "Information Technology (IT)": "jjjei_admin:3.css",
+    "Legal": "jjjei_admin:4.css",
+    "Office Management (OM)": "jjjei_admin:5.css",
+  },
   "Facilities": {
     "Base Color": "jjjei_facilities:0.css",
     "Architectural, Civil, and Structural (ACS)": "jjjei_facilities:1.css",
@@ -8,19 +16,16 @@ export const colorGroups: Record<string, Record<string, string>> = {
     "Plumbing": "jjjei_facilities:5.css",
     "Project (PJT)": "jjjei_facilities:4.css",
   },
-  "Admin": {
-    "Base Color": "jjjei_admin:0.css",
-    "Building and Land Administration (BLA)": "jjjei_admin:1.css",
-    "Compliance": "jjjei_admin:2.css",
-    "Information Technology (IT)": "jjjei_admin:3.css",
-    "Legal": "jjjei_admin:4.css",
-    "Office Management (OM)": "jjjei_admin:5.css",
-  },
   "GMO": {
     "Base Color": "jjjei_gmo:0.css",
     "Audit": "jjjei_gmo:1.css",
     "Automation": "jjjei_gmo:2.css",
     "Streamlining": "jjjei_gmo:3.css",
+  },
+  "HR": {
+    "Base Color": "jjjei_hr:0.css",
+    "Operations": "jjjei_hr:1.css",
+    "Payroll and Benefits": "jjjei_hr:2.css",
   },
   "Leasing": {
     "Base Color": "jjjei_leasing:0.css",
@@ -34,21 +39,16 @@ export const colorGroups: Record<string, Record<string, string>> = {
     "Receivables": "jjjei_controller:2.css",
     "Tax and Compliance" : "jjjei_controller:3.css",
   },
+  "Procurement and Inventory": {
+    "Base Color": "jjjei_procinv:0.css",
+    "Inventory and Warehousing": "jjjei_procinv:2.css",
+    "Procurement": "jjjei_procinv:1.css",
+  },
   "Treasury": {
     "Base Color": "jjjei_treasury:0.css",
     "Billing": "jjjei_treasury:1.css",
     "Collection": "jjjei_treasury:2.css",
     "Disbursement": "jjjei_treasury:3.css",
-  },
-  "HR": {
-    "Base Color": "jjjei_hr:0.css",
-    "Operations": "jjjei_hr:1.css",
-    "Payroll and Benefits": "jjjei_hr:2.css",
-  },
-  "Procurement and Inventory": {
-    "Base Color": "jjjei_procinv:0.css",
-    "Inventory and Warehousing": "jjjei_procinv:2.css",
-    "Procurement": "jjjei_procinv:1.css",
   },
 };
 
